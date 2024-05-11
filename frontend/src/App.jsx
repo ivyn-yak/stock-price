e642f62559536074a6b44ac1031be0c315cbfad1
Ivyn Yak <ivyn.yak.2022@scis.smu.edu.sg>
2024-05-12 03:09:53 +0800

    config tailwind

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import Line from './components/LineGraph'
+import "./App.css"
 
 const App = () => {
   return (
-    <div >
+    <div className='w-full flex justify-center p-10'>
       <Line />
     </div>
   )
